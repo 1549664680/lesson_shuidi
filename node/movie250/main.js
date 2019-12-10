c863d52ffccc1298ab3ac516a02616aed6075e20
1549664680 <1549664680@qq.com>
2019-12-10 21:59:30 +0800

    pachon

--- node/movie250/main.js
+++ node/movie250/main.js
@@ -2,39 +2,37 @@ const request = require('request-promise')
 const cheerio = require('cheerio')
 const fs = require('fs')
 const main= async() => {
-    //进程
-    let html = await request({
-        url:'https://movie.douban.com/top250'
-        // https://movie.douban.com/top250?start=0&filter=
-
-    })
-    // console.log(html)
-    let $ = cheerio.load(html)//内存之中构建dom $就相当于document.querySelector
-    let movieNodes =  $('#content .article .grid_view').find('.item')   
-    // console.log(movieNodes);
-    let movies = [] 
-    for (let i = 0; i < movieNodes.length; i++){
-        let item = cheerio.load(movieNodes[i])
-        let titles = $('.info .hd span')
-        titles = ([]).map.call(titles,t =>{
-            return $(t).text()
+    let j=1;
+    for (let i = 0; i <= 225; i+=25){
+        let html = await request({
+            url:'https://movie.douban.com/top250?start='+i+'&filter='
         })
-        // console.log(titles);
-        let bd = $('.info .bd')
-        let info = bd.find('p').text()
-        let score = bd.find('.star .rating_num').text();
-        movies.push({
-            'titles': titles,
-            'info':info,
-            'score':score
+        let $ = cheerio.load(html)//内存之中构建dom $就相当于document.querySelector
+        let movieNodes =  $('#content .article .grid_view').find('.item')   
+        let movies = [] 
+        for (let i = 0; i < movieNodes.length; i++){
+            let item = cheerio.load(movieNodes[i])
+            let titles = $('.info .hd span')
+            titles = ([]).map.call(titles,t =>{
+                return $(t).text()
+            })
+            let bd = $('.info .bd')
+            let info = bd.find('p').text()
+            let score = bd.find('.star .rating_num').text();
+            movies.push({
+                'titles': titles,
+                'info':info,
+                'score':score
+            })
+        }
+        console.log(movies)
+        fs.writeFile('./output'+j+'.json',JSON.stringify(movies),'utf-8',(err)=>{
+            if(err){
+                console.log('写入失败')
+            } console.log('生成json文件成功，爬取完成')
+    
         })
+        j++
     }
-    console.log(movies)
-    fs.writeFile('./output.json',JSON.stringify(movies),'utf-8',(err)=>{
-        if(err){
-            console.log('写入失败')
-        } console.log('生成json文件成功，爬取完成')
-
-    })
 }
 main();
