1ebfcaa12a52071b2c611b6c4c170d04e973c7ab
1549664680 <1549664680@qq.com>
2019-12-13 21:36:12 +0800

    koa

---
+++ koa/blog/config/default.js
@@ -0,0 +1,11 @@
+const config = {
+    port:3000,
+    database:{
+        DATABASE:'nodesql',
+        USERNAME:'root',
+        PASSWORD:'1234567890',
+        port:'3306',
+        HOST:'192.168.31.128'
+    }
+}
+module.exports = config
