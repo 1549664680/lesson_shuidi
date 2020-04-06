40e30246c3bbeda39f11648cba1255d05f6326c5
1549664680 <1549664680@qq.com>
2020-04-06 10:57:37 +0800

    72距离编辑

---
+++ leetcode/72.编辑距离/1.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function(word1, word2) {
+  var n = word1.length;
+  var m = word2.length;
+  var minDist = Number.MAX_SAFE_INTEGER;
+  function helper(i,j,edist){
+      // terminator
+      if(i == n || j == m){
+          // 没有剩余字符的单词要加上有剩余字符单词的剩余字符个数
+          if(i < n){
+              edist+=(n-i);
+          }
+          if(j < m){
+              edist+=(m-j);
+          }
+          minDist = Math.min(edist,minDist);
+          // console.log(minDist)
+          return;
+      }
+      if(word1[i] == word2[j]){   // 相同的话都往前走一步，步数也不变
+          helper(i+1,j+1,edist);
+      }else{
+        // 删除a[i] 或 b[j]前添加一个字符
+        helper(i+1,j,edist+1);
+        // 删除b[j] 或 a[i]前添加一个字符
+        helper(i,j+1,edist+1);
+        // 将a[i]和b[j]替换为相同字符
+        helper(i+1,j+1,edist+1);
+      }
+  }
+  helper(0,0,0);
+  return minDist;
+};
+minDistance("horse","ros")
+/// 递归回溯， 结果超时
---
+++ leetcode/72.编辑距离/2.js
@@ -0,0 +1,49 @@
+// 解法二：动态规划+自底向上-I.
+var minDistance = function(word1, word2) {
+  if(word1 == word2){
+      return 0;
+  }
+  // 行
+  var n = word1.length;
+  // 列
+  var m = word2.length;
+  if(!n || !m){
+      return n || m;
+  }
+  var minDist = Array.from(new Array(n),() => new Array(m));
+  // 初始化第1列第n行的数据
+  for(var i = 0;i < n;++i){
+      if(word1[i] == word2[0]){
+          minDist[i][0] = i;
+      }else{
+          if(i == 0){
+              minDist[i][0] = 1;
+          }else{
+              minDist[i][0] = minDist[i-1][0] + 1;
+          }
+      }
+  }
+  // 初始化第1行第n列的数据
+  for(var j = 0;j < m;++j){
+      if(word1[0] == word2[j]){
+          minDist[0][j] = j;
+      }else{
+          if(j == 0){
+              minDist[0][j] = 1;
+          }else{
+              minDist[0][j] = minDist[0][j-1] + 1;
+          }
+      }
+  }
+  for(var i = 1;i < n;++i){
+      for(var j = 1;j < m;++j){
+          if(word1[i] == word2[j]){
+              minDist[i][j] = Math.min(minDist[i-1][j]+1,minDist[i][j-1]+1,minDist[i-1][j-1]);
+          }else{
+              minDist[i][j] = Math.min(minDist[i-1][j]+1,minDist[i][j-1]+1,minDist[i-1][j-1]+1);
+          }
+      }
+  }
+  return minDist[n-1][m-1];
+};
+console.log(minDistance("horse","ros"))
