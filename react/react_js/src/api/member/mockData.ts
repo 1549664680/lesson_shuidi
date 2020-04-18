f29e55a9bef26996807565113b14839e08b7b69b
1549664680 <1549664680@qq.com>
2020-04-18 11:32:36 +0800

    react_js

---
+++ react/react_js/src/api/member/mockData.ts
@@ -0,0 +1,14 @@
+// es6  vue 使用ts 写出来， 编译的
+import { MemberEntity } from '../../model'
+export const members: MemberEntity[] = [
+  {
+    id: 123,
+    login: 'lan',
+    avatar_url: 'https://assets.leetcode-cn.com/aliyun-lc-upload/default_avatar.png'
+  },
+  {
+    id: 456,
+    login: 'jinron',
+    avatar_url: 'https://assets.leetcode-cn.com/aliyun-lc-upload/default_avatar.png'
+  }
+]
