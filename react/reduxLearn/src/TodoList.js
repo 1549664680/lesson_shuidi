f6aa978729c219b1803e71bb1b3100479aec9048
1549664680 <1549664680@qq.com>
2020-04-29 21:30:35 +0800

    new

--- react/reduxLearn/src/TodoList.js
+++ react/reduxLearn/src/TodoList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import 'antd/dist/antd.css'
-import { Input, Button, List } from 'antd'
+import TodoListUI from './TodoListUI'
 import store from './store'
 import {changeInputvalue,clickBtn,deleteItem} from './store/actionCreates'
 class TodoList extends Component {
@@ -10,32 +9,18 @@ constructor(props) {
   this.changeInputvalue = this.changeInputvalue.bind(this)
   this.clickBtn = this.clickBtn.bind(this)
   this.storeChange = this.storeChange.bind(this)
+  this.deleteItem = this.deleteItem.bind(this)
   store.subscribe(this.storeChange)
 }
 render() {
   return (
-    <div style={{ margin: '10px' }}>
-      <div>
-        <Input
-          placeholder={this.state.inputVlue}
-          style={{ width: '250px', marginRight: '10px' }}
-          onChange={this.changeInputvalue}
-          id = 'input'
-          value = {this.state.inputVlue}
-        />
-        <Button
-          type="primary"
-          onClick={this.clickBtn}
-        >增加</Button>
-      </div>
-      <div>
-        <List
-          bordered
-          dataSource={this.state.list}
-          renderItem={(Item, index) => (<List.Item onClick={this.deleteItem.bind(this, index)}>{Item}</List.Item>)}
-        />
-      </div>
-    </div>
+    <TodoListUI
+    inputValue = {this.state.inputValue}
+    changeInputvalue={this.changeInputvalue}
+    clickBtn={this.clickBtn}
+    list={this.state.list}
+    deleteItem ={this.deleteItem}
+    />
   );
 }
 storeChange() {
--- react/reduxLearn/src/store/reducer.js
+++ react/reduxLearn/src/store/reducer.js
@@ -1,7 +1,7 @@
 import { CHANGE_INPUT, ADDITEM, DELETEITEM } from './actionType'
 
 const defaultState = {
-  inputVlue: 'Write Something',
+  inputValue: 'Write Something',
   list: [
     '早上开会',
     '中午睡觉',
@@ -13,13 +13,13 @@ export default (state = defaultState, action) => {
   // reducer 里只能接受state ，不能改变state
   if (action.type === CHANGE_INPUT) {
     let newState = JSON.parse(JSON.stringify(state))
-    newState.inputVlue = action.value
+    newState.inputValue = action.value
     return newState
   }
   if (action.type === ADDITEM) {
     let newState = JSON.parse(JSON.stringify(state))
-    newState.list.push(newState.inputVlue)
-    newState.inputVlue = ' '
+    newState.list.push(newState.inputValue)
+    newState.inputValue = ''
     return newState
   }
   if (action.type === DELETEITEM) {
