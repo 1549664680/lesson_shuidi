d0b9d34a7f737f7d24d170b709bb7cf88a42efac
1549664680 <1549664680@qq.com>
2020-05-18 09:41:48 +0800

    152

--- leetcode/152.乘积最大数组/1.js
+++ leetcode/152.乘积最大数组/1.js
@@ -1,10 +1,11 @@
 var maxProduct = function (nums) {
   let max = 1
   let min = 1
-  let res = Number.MIN_VALUE
-  console.log(res)
+  let res = (Number.MAX_VALUE)*(-1)
   for (let i = 0; i < nums.length; i++) {
     if(nums[i]<0){
+      // 如果是负数，最大值和最小值交换
+      // 这样max就拿到最小值，也就是负数，然后与负数nums[i]相乘
       [max,min] = [min,max]
     }      
     max = Math.max(max*nums[i],nums[i])
